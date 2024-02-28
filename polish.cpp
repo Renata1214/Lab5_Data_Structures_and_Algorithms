8968695bfa0be3df73cda470fdb77d65a1da963c
Renata Espinosa <114301354+Renata1214@users.noreply.github.com>
2024-02-28 14:22:45 +0000

    Changes

---
+++ polish.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <stack>
+
+using namespace std;
+
+//Function to evaluate postfix expressions
+
+int evaluate_postfix (string expression){
+    char c;
+    int sizeString=0;
+    stack 
+    while (sizeString!= expression.size()){
+
+        char=expression[sizeString];
+
+
+        sizeString++;
+    }
+
+
+}
