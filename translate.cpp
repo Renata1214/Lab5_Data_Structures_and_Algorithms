36d635a139cac6c938acc2d58e529d037e1e99b8
Renata Espinosa <114301354+Renata1214@users.noreply.github.com>
2024-02-28 19:28:27 +0000

    Changes

---
+++ translate.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cmath>
+#include <stack>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+/*Your program must be able to handle the following operations: +,-,*,/,%
+ BNF (Backus–Naur form)1 of the infix expression grammar is:
+expr ::= term | expr op1 term
+op1 ::= + | -
+term ::= number | term op2 number
+op2 ::= * | / | %
+*/
+//Function to translate code infix into postfix
+string translation (string infix1){
+
+    string line; //postfix line
+    char charac;
+    int iterator=0;
+    int number;
+    stack mystack;
+
+    while (iterator!= infix1.size()){
+       charac=infix[iterator];
+        if (isdigit(charac)){
+            line.append(charac);
+        }
+        else {
+            if(charac=='*' || charac == '/' || charac == '%'){
+                bool check=true;
+                while (check){
+                    if (mystack.top()=='/'|| mystack.top()=='%'|| mystack.top()=='*'){
+                        line.append(mystack.top());
+                        mystack.pop;
+                        }
+                    else {
+                        mystack.push(charac);
+                        check = false;
+                    }
+                }
+            }
+
+            if(charac=='+' || charac == '-'){
+                bool check2=true;
+                    while (check2){
+                        if (mystack.top()=='/'|| mystack.top()=='%'|| mystack.top()=='*'|| mystack.top()=='+'|| mystack.top()=='-'){
+                            line.append(mystack.top());
+                            mystack.pop;
+                            }
+                        else {
+                            mystack.push(charac);
+                            check2 = false;
+                        }
+                    }
+            }
+                    
+        }
+    }
+    return line;
+}
+
+void 
